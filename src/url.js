118df36c54309cf87bcbfbf8d8ee32976de4edac
elguesabal <joseelguesabal@gmail.com>
2024-09-06 23:05:34 -0300

    trocando a url da api da local para o deploy do onrender

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const url = "http://192.168.0.110:3000";
-// export const url = "https://api-7-circuito-badbons-open.onrender.com";
+// export const url = "http://192.168.0.110:3000";
+export const url = "https://api-torneio-da-independencia.onrender.com";
