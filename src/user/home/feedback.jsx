a81ee58f3a43b12d8c33a89ac36612d8299e585a
elguesabal <joseelguesabal@gmail.com>
2024-09-08 08:19:50 -0300

    removendo log de depuracao

--- src/user/home/feedback.jsx
+++ src/user/home/feedback.jsx
@@ -12,7 +12,7 @@ export default function Feedback() {
 		inicioLoad();
 		axios.post(`${url}/feedback`, feedback)
 		.then((res) => alert("Feedback enviado!"))
-		.catch((erro) => { console.log(erro), alert("Falha ao enviar feedback 😢") });
+		.catch((erro) => alert("Falha ao enviar feedback 😢"));
 		fimLoad();
 	}
 
